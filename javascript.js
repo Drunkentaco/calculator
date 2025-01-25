22eb05100cbd080fea461e3b8d19154ca492af14
drunkentaco <134489107+Drunkentaco@users.noreply.github.com>
2025-01-25 15:26:46 -0500

    Added ifs for on '0's and '.' for it to work properly (no display showing '00034' or 0.344.23)

--- javascript.js
+++ javascript.js
@@ -1,19 +1,20 @@
-
 let firstNumber = '';
 let secondNumber = '';
 let operator = '';
+let resetSwitich = false
+let mathFormula = [];
 
 
 //button selectors
 const clear = document.querySelector('#clearBtn');
-clear.addEventListener('click', () =>{
-    clearAll()
-});
+clear.addEventListener('click', () =>{clearAll()});
+
 const delBtn = document.querySelector('#deleteBtn');
-delBtn.addEventListener('click', () => {
-    deleteNumber()
-});
+delBtn.addEventListener('click', () => {deleteNumber()});
+
 const equal = document.querySelector('#equalBtn');
+equal.addEventListener('click', () => {calculate()});
+
 const numberBtn = document.querySelectorAll('.numberBtn');
 numberBtn.forEach((button) => {
     button.addEventListener('click',() => {
@@ -21,20 +22,26 @@ numberBtn.forEach((button) => {
     });
 });
 const operatorBtn = document.querySelectorAll('.operator');
-    operatorBtn.forEach((button) =>{
-        button.addEventListener('click',() =>{
-            // Function?
-        })
+operatorBtn.forEach((button) =>{
+    button.addEventListener('click',() =>{
+        numberStorage(button.name)
     })
+})
 const display = document.querySelector('#display');
-
 display.textContent = 0
+
 function displayNumber(num) {
-   // Keep numbers to 10 digits
+    if (resetSwitich === true){
+        clearAll();
+        resetSwitich = false
+    }
+    // Keep numbers to 10 digits
     if (display.textContent.length < 10){
+    //Ensure that you don't add another '0' on top of the '0'
     if(display.textContent === '0'){
         deleteNumber()
     }
+    //adds a '0' after the '.'
     if (num === '.'){
         if (display.textContent.includes('.')) return;
         if (display.textContent.length <= 0){
@@ -46,39 +53,37 @@ function displayNumber(num) {
 }
 
 function deleteNumber (){
-    num = display.textContent
+    num = display.textContent;
     display.textContent = num.slice(0,-1);
 }
 
 function clearAll(){
-    display.textContent = '0'
+    display.textContent = '0';
 }
 
-/* let oper = operators.forEach((button) => {
-    button.addEventListener('click', () => {
-        const display = document.querySelector('#display');   
-        if (firstNumber === ''){
-            firstNumber = display;
-            console.log(`${firstNumber}`)
-        }
-        else if (secondNumber === ''){
-           console.log('2nd' `${display}`)
-        }
-    });
-}); */
+function numberStorage (oper){
+    if (firstNumber === ''){
+        firstNumber = display.textContent;
+        operator = oper;
+        resetSwitich = true
+    } else if (secondNumber === ''){
+        secondNumber = display.textContent;
+        resetSwitich = true
+        calculate();
+    }
+}
 
 
 // Basic arithmetic operation
-const operate = function (firstNumber, operator, secondNumber){
-    if (oper = '+'){
-        return firstNumber + secondNumber;
+function calculate (firstNumber, secondNumber){
+    if (operator = '+'){
+        answer = firstNumber + secondNumber;
+        display.textContent = answer;
     } else if (operator = '-'){
         return firstNumber - secondNumber;
-    } else if (operator = '*'){
+    } else if (operator = 'x'){
         return firstNumber * secondNumber;
     }else if (operator = '/'){
         return firstNumber / secondNumber;
     }else console.log('operate error');
 }
-
-
